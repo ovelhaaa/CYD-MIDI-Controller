ef455dc3aee35a452d350c7eb14e751bbea537e6
ovelhaaa <ovelhaaa@gmail.com>
2026-07-24 19:11:16 -0300

    Update cyd_touch.cpp

--- cyd_touch.cpp
+++ cyd_touch.cpp
@@ -1,18 +1,16 @@
 #include "cyd_touch.h"
 
 namespace {
-constexpr uint8_t CMD_Z1 = 0xB1;
-constexpr uint8_t CMD_Z2 = 0xC1;
-constexpr uint8_t CMD_X = 0x91;
-constexpr uint8_t CMD_Y = 0xD1;
+constexpr uint8_t CMD_Z1 = 0xB0;
+constexpr uint8_t CMD_Z2 = 0xC0;
+constexpr uint8_t CMD_X = 0x90;
+constexpr uint8_t CMD_Y = 0xD0;
 
 void touchDelay() {
   delayMicroseconds(1);
 }
 
-uint16_t transfer16(uint8_t command) {
-  uint16_t value = 0;
-
+void sendCommand(uint8_t command) {
   for (int bit = 7; bit >= 0; --bit) {
     digitalWrite(CYD_TOUCH_CLK, LOW);
     digitalWrite(CYD_TOUCH_MOSI, (command >> bit) & 0x01);
@@ -20,7 +18,12 @@ uint16_t transfer16(uint8_t command) {
     digitalWrite(CYD_TOUCH_CLK, HIGH);
     touchDelay();
   }
+}
 
+uint16_t read16() {
+  uint16_t value = 0;
+
+  digitalWrite(CYD_TOUCH_MOSI, LOW);
   for (int bit = 15; bit >= 0; --bit) {
     digitalWrite(CYD_TOUCH_CLK, LOW);
     touchDelay();
@@ -33,6 +36,16 @@ uint16_t transfer16(uint8_t command) {
   }
 
   return value;
+}
+
+int16_t readAdc(uint8_t command) {
+  digitalWrite(CYD_TOUCH_CS, LOW);
+  touchDelay();
+  sendCommand(command);
+  uint16_t value = read16();
+  digitalWrite(CYD_TOUCH_CS, HIGH);
+
+  return (value >> 3) & 0x0FFF;
 }
 
 int16_t bestTwoAverage(int16_t a, int16_t b, int16_t c) {
@@ -61,26 +74,18 @@ bool cydTouchRead(CYDTouchPoint &point) {
   int16_t x[3] = {};
   int16_t y[3] = {};
 
-  digitalWrite(CYD_TOUCH_CS, LOW);
-  touchDelay();
-
-  int16_t z1 = transfer16(CMD_Z1) >> 3;
-  int16_t z2 = transfer16(CMD_Z2) >> 3;
+  int16_t z1 = readAdc(CMD_Z1);
+  int16_t z2 = readAdc(CMD_Z2);
   int16_t z = z1 + 4095 - z2;
 
   if (z >= CYD_TOUCH_Z_THRESHOLD) {
-    transfer16(CMD_X);
-    x[0] = transfer16(CMD_X) >> 3;
-    y[0] = transfer16(CMD_Y) >> 3;
-    x[1] = transfer16(CMD_X) >> 3;
-    y[1] = transfer16(CMD_Y) >> 3;
-    x[2] = transfer16(CMD_X) >> 3;
-    y[2] = transfer16(CMD_Y) >> 3;
+    x[0] = readAdc(CMD_X);
+    y[0] = readAdc(CMD_Y);
+    x[1] = readAdc(CMD_X);
+    y[1] = readAdc(CMD_Y);
+    x[2] = readAdc(CMD_X);
+    y[2] = readAdc(CMD_Y);
   }
-
-  transfer16(0x00);
-
-  digitalWrite(CYD_TOUCH_CS, HIGH);
 
   if (z < CYD_TOUCH_Z_THRESHOLD) {
     point.z = 0;
